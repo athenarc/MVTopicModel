17353429f08e5a0eceaf659613f3cbc0e4e25311
npit <npittaras@di.uoa.gr>
2019-05-01 23:21:27 +0300

    Add spring rest module

---
+++ MVTopicModelRestAPI/src/main/java/org/madgik/config/WebAppConfig.java
@@ -0,0 +1,21 @@
+package org.madgik.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
