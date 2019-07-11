b1bdb81622b084d57c26c664f19b552f1fa4b5d2
npit <npittaras@di.uoa.gr>
2019-07-11 10:03:25 +0000

    Update endpoint paths

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 
+# Paths should ne already set at the application properties
+# keeping these here in case it needs to be automated more in the future.
 trained_model_path="path/to/trained/model"
 topics_information_query="queries/getTopicsInformation.sql"
+document_topics_information_query="queries/getDocumentsPerTopic.sql"
+document_visuzalized_details_query="queries/documentVisualizationInfo.sql"
 
+  
 tomcat_path="tomcat"
 tomcat_url="https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.42/bin/apache-tomcat-8.5.42.tar.gz"
 endpoint_name="mvtm_api"
@@ -22,11 +27,14 @@ if [ ! -f "${tomcat_path}/bin/startup.sh" ]; then
 		&& mv "${unzipped}"/* ./ && rm -r "${unzipped}" ${zipped} \
 		&& cd "${cwd}"
 	echo "Done"
+else
+	echo "Shutting down existing server..."
+	${tomcat_path}/bin/shutdown.sh
 fi
 
 # modify restful pom
-echo "Setting model path to ${trained_model_path}"
-sed -i "s|<pom.model.path>.*|<pom.model.path>${trained_model_path}</pom.model.path>|" MVTopicModelRestAPI/pom.xml
+echo "Assumes property paths are set"
+# sed -i "s|<pom.model.path>.*|<pom.model.path>${trained_model_path}</pom.model.path>|" MVTopicModelRestAPI/pom.xml
 
 # build and copy the war
 echo "Building"
@@ -38,4 +46,4 @@ cp MVTopicModelRestAPI/target/MVTopicModelRestAPI.war "${tomcat_path}/webapps/${
 echo "Starting tomcat. Logs @ $(ls -t ${tomcat_path}/logs | head -1)"
 "${tomcat_path}"/bin/startup.sh
 
-echo "Endpoint name: ${endpoint_name}. Try http://${ip_address}:8080/${endpoint_name}/hello"
+echo "Endpoint name: ${endpoint_name}. Try \"curl http://${ip_address}:8080/${endpoint_name}/hello\""
